c1a0384fcbd6b1b7dbf1eb79902fc6b86e577ffd
smooth97 <dbtmdwls0506@naver.com>
2019-11-11 17:30:59 +0900

    vue conditionals

--- app.js
+++ app.js
@@ -1,33 +1,56 @@
 new Vue({
   el: "#vue-app",
   data: {
-    name: "Smooth",
-    job: "developer",
-    website: "https://velog.io/@smooth97/Vue.js",
-    websiteTag: '<a href="https://velog.io/@smooth97/Vue.js">Blog</a>',
-    age: 23,
-    x: 0,
-    y: 0
+    available: false,
+    nearby: false,
+    error: false,
+    success: false
   },
   methods: {
-    greet: function(time) {
-      return `Good ${time} ${this.name}`;
-    },
-    add: function() {
-      this.age++;
-    },
-    substract: function() {
-      this.age--;
-    },
-    updateXY: function(event) {
-      this.x = event.offsetX;
-      this.y = event.offsetY;
-    },
-    logName: function(e) {
-      console.log(e.target.value);
-    },
-    logAge: function(e) {
-      console.log(e.target.value);
-    }
-  }
+  
+  },
+  
 });
+
+
+// new Vue({
+//   el: "#vue-app",
+//   data: {
+//     name: "Smooth",
+//     job: "developer",
+//     website: "https://velog.io/@smooth97/Vue.js",
+//     websiteTag: '<a href="https://velog.io/@smooth97/Vue.js">Blog</a>',
+//     age: 23,
+//     x: 0,
+//     y: 0
+//   },
+//   methods: {
+//     greet: function(time) {
+//       return `Good ${time} ${this.name}`;
+//     },
+//     add: function() {
+//       this.age++;
+//     },
+//     substract: function() {
+//       this.age--;
+//     },
+//     updateXY: function(event) {
+//       this.x = event.offsetX;
+//       this.y = event.offsetY;
+//     },
+//     logName: function(e) {
+//       console.log(e.target.value);
+//     },
+//     logAge: function(e) {
+//       console.log(e.target.value);
+//     }
+//   },
+  // computed: {
+  //   compClasses: function(){
+  //     return {
+  //       available: this.available,
+  //       nearby: this.nearby
+  //     }
+  //   }
+  // }
+// });
